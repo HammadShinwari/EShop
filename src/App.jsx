4ba7db73cfddc0b4cd2ebcf499bbf4481dcddd05
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-04-16 19:47:59 +0500

    EShop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import files
+import Home from './pages/home/Home';
+import Order from './pages/order/Order';
+import Cart from './pages/cart/Cart';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import NoPage from './pages/nopage/NoPage';
+
 
 function App() {
   return (
-    <div>
-        <div className='text-2xl'>Hammad</div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/order" element={<Order/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/*" element={<NoPage/>} />
+      </Routes>
+    </Router>
   )
 }
 
