0030b137af2353eeb7acfda4e6c37dd73779a8cf
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-04-16 21:13:04 +0500

    EShop

---
+++ src/context/data/myState.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import files
+import MyContext from './myContext';
+
+function MyState(props) {
+    const state = {
+        name: "Kamal Nayan",
+        class: "9 C"
+    }
+  return (
+    <MyContext.Provider value={state}>
+       {props.children}
+    </MyContext.Provider>
+  )
+}
+
+export default MyState;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import Layout from '../../components/layout/Layout';
+import React, { useContext } from 'react'
+import Layout from '../../components/layout/Layout'
+import myContext from '../../context/data/myContext'
 
 function Home() {
+  const context = useContext(myContext)
+  console.log(context) // {name: 'Kamal Nayan', class: '9 C'}
+  // Destructure 
+  const {name} = context
+  console.log(name) // Kamal Nayan
   return (
-    <Layout>Home</Layout>
+    <Layout>
+      <h1>Name : {name}</h1>
+    </Layout>
   )
 }
 
-export default Home;
+export default Home
