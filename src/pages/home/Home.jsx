aa275828f5ae50b4308100f26007bc0c967550f4
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-04-26 21:44:07 +0500

    EShop

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,11 +5,31 @@ import Filter from '../../components/filter/Filter';
 import ProductCard from "../../components/productCard/ProductCard";
 import Track from '../../components/track/Track';
 import Testimonial from "../../components/testimonial/Testimonial";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, deleteFromCart } from "../../redux/cartSlice";
 
 function Home() {
+
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart);
+
+  console.log(cartItem);
+
+  const addCart = () => {
+    dispatch(addToCart("shirt"));
+  }
+
+  const deleteCart = () => {
+    dispatch(deleteFromCart("shirt"));
+  }
+
   return (
     <div>
       <Layout>
+        <div className="flex gap-5 justify-center">
+          <button className="bg-gray-300 p-2" onClick={() => addCart()}>add</button>
+          <button className="bg-gray-300 p-2" onClick={() => deleteCart()}>del</button>
+        </div>
         <HeroSection />
         <Filter />
         <ProductCard />
