e00cd769f4e210c8f1e03c93c170596c2ec31eb0
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-04-27 17:21:03 +0500

    EShop

---
+++ src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRVblu3_HuhSoVrkzXYuGiaEcsSGoa-tU",
+  authDomain: "myfirstapp-816fa.firebaseapp.com",
+  projectId: "myfirstapp-816fa",
+  storageBucket: "myfirstapp-816fa.appspot.com",
+  messagingSenderId: "1054989612658",
+  appId: "1:1054989612658:web:a6153067efb49c8ff6b4eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
