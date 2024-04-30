ba3503bc475b6b97e1e01f8f73020d116ea05040
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-04-30 20:28:07 +0500

    Incomplete Website

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRVblu3_HuhSoVrkzXYuGiaEcsSGoa-tU",
-  authDomain: "myfirstapp-816fa.firebaseapp.com",
-  projectId: "myfirstapp-816fa",
-  storageBucket: "myfirstapp-816fa.appspot.com",
-  messagingSenderId: "1054989612658",
-  appId: "1:1054989612658:web:a6153067efb49c8ff6b4eb"
+  apiKey: "AIzaSyBwb_bGb58ifBb2iJzu-gmbt-y-OveTvzI",
+  authDomain: "eshop-28209.firebaseapp.com",
+  projectId: "eshop-28209",
+  storageBucket: "eshop-28209.appspot.com",
+  messagingSenderId: "701229033996",
+  appId: "1:701229033996:web:9e3240dd3c93af1fd4dc29"
 };
 
 // Initialize Firebase
